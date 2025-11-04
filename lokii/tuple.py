58508ef2656a30cd489f64d5aee9aaa529e1138b
Lokiii <lokeshvallapu1821@gmail.com>
2025-11-04 23:03:01 +0530

    update1

--- lokii/tuple.py
+++ lokii/tuple.py
@@ -1,7 +1,11 @@
 #Tuple tutorial 
+#Accessing the element from tuple
 a=(1,2,3,"loki","mahi",34)
 print(a)
 print(a[2])
 print(a[:3])
 print(a[2:])
+print(a[3:9])
 print(a[-1])
+print(a[:-1])
+print(a[0])
